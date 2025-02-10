bc75850bfdb83ad513da86fe41758409c643940e
Joel Gustafson <joelgustafson@protonmail.com>
2025-02-10 15:34:44 -0500

    forward standard optimize option

--- build.zig
+++ build.zig
@@ -50,10 +50,10 @@ pub fn build(b: *std.Build) !void {
         .root_source_file = b.path("src/Quadtree.zig"),
     });
 
-    addTarget(b, "x86_64-linux/libquadtree.so", .{ .cpu_arch = .x86_64, .os_tag = .linux });
-    addTarget(b, "aarch64-linux/libquadtree.so", .{ .cpu_arch = .aarch64, .os_tag = .linux });
-    addTarget(b, "x86_64-macos/libquadtree.dylib", .{ .cpu_arch = .x86_64, .os_tag = .macos });
-    addTarget(b, "aarch64-macos/libquadtree.dylib", .{ .cpu_arch = .aarch64, .os_tag = .macos });
+    addTarget(b, "x86_64-linux/libquadtree.so", .{ .cpu_arch = .x86_64, .os_tag = .linux }, optimize);
+    addTarget(b, "aarch64-linux/libquadtree.so", .{ .cpu_arch = .aarch64, .os_tag = .linux }, optimize);
+    addTarget(b, "x86_64-macos/libquadtree.dylib", .{ .cpu_arch = .x86_64, .os_tag = .macos }, optimize);
+    addTarget(b, "aarch64-macos/libquadtree.dylib", .{ .cpu_arch = .aarch64, .os_tag = .macos }, optimize);
 
     // Tests
     const tests = b.addTest(.{ .root_source_file = b.path("src/test.zig") });
@@ -61,11 +61,11 @@ pub fn build(b: *std.Build) !void {
     b.step("test", "Run tests").dependOn(&run_tests.step);
 }
 
-fn addTarget(b: *std.Build, name: []const u8, target: std.Target.Query) void {
+fn addTarget(b: *std.Build, name: []const u8, target: std.Target.Query, optimize: std.builtin.OptimizeMode) void {
     const lib = b.addSharedLibrary(.{
         .name = "quadtree",
         .target = b.resolveTargetQuery(target),
-        .optimize = .ReleaseSafe,
+        .optimize = optimize,
         .root_source_file = b.path("lib/quadtree.zig"),
     });
 
